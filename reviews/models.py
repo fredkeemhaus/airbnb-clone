c958c81b0aecb3f76b3e5d128cc517c8b40eb0cf
fredkeemhaus <fredkeemhaus@gmail.com>
2020-08-18 00:39:35 +0900

    all other apps done

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.TimeStapedModel):
+    """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.Inaccuracy = models.IntegerField()
+    cleanliness = models.Inaccuracy = models.IntegerField()
+    loaction = models.Inaccuracy = models.IntegerField()
+    check_in = models.Inaccuracy = models.IntegerField()
+    value = models.Inaccuracy = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"
