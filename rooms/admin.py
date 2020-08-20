d181af443bf933776b91b8d9f434e49cb2cdb45f
fredkeemhaus <fredkeemhaus@gmail.com>
2020-08-20 15:37:43 +0900

    admin filter

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,7 +13,58 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price"),},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "house_rules",
+        "facilities",
+        "city",
+        "country",
+    )
+
+    search_fields = ("city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return "Potato"
+
+    count_amenities.short_description = "hello sexy"
 
 
 @admin.register(models.Photo)
